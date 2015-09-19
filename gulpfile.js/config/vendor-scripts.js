e64e51f10637cdb65657445dd318baab8ffe839b
Erick Bessa <droidebessa@gmail.com>
2015-09-19 15:14:09 -0300

    fix missing separator in a gulp taks

--- gulpfile.js/config/vendor-scripts.js
+++ gulpfile.js/config/vendor-scripts.js
@@ -3,7 +3,7 @@ var config = require('./')
 module.exports = {
   rootSrc: config.sourceAssets + '/js/libs/',
   src: [
-    config.sourceAssets + '/js/libs/angular.js'
+    config.sourceAssets + '/js/libs/angular.js',
     config.sourceAssets + '/js/libs/angular-ui-router.js'
   ],
   dest: config.publicAssets + '/js'
